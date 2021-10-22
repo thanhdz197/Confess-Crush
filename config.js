b53e5cd225d759ff901ecd542ce70803a0bd584f
thanhdz197 <92968501+thanhdz197@users.noreply.github.com>
2021-10-22 12:13:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    titleWeb: "Web này dành cho em <3",
+    introTitle: 'From to THÀNH Đz',
+    introDesc: `Anh đây chẳng biết làm thơ
+    Em cho anh hỏi ăn cơm mấy giờ
+    Một thương hai đợi ba chờ
+    Chờ duyên chờ phận chờ về ăn cơm`,
+    btnIntro: 'HEHE:v',
+    title: 'Tuyết tan mưa tạnh hoa tàn 🥰',
+    desc: 'Tắt đèn đi ngủ thế là yêu nhau :v ',
+    btnYes: 'LOVE U <3',
+    btnNo: 'Từ chối yêu',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi câu trả lời',
+    reply: 'Em yêu anh từ cái nhìn đầu tiên.con tim em như chết lặng',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h, anh qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
